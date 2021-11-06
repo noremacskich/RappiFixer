bfa95e6a02ed0a5a5e245d3c539da32cd73d04a7
NoremacSkich <noremacskich@gmail.com>
2021-11-06 17:11:57 -0500

    App should now be able to handle both utf-8 and utf-8 with bom

--- RappiFixer/Models/ProductCost.cs
+++ RappiFixer/Models/ProductCost.cs
@@ -6,6 +6,7 @@ namespace RappiFixer.Models
 {
     public class ProductCost
     {
+        //public string N { get; set; }
         public string PROMOCION { get; set; }
         public double GANACIA { get; set; }
     }
--- RappiFixer/RappiOrderApp.cs
+++ RappiFixer/RappiOrderApp.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace RappiFixer
 {
@@ -105,15 +106,57 @@ namespace RappiFixer
                 throw new FileNotFoundException("unable to find the csv file", fileLocation);
             }
 
-            using StreamReader reader = new StreamReader(fileLocation);
-            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var utf8NoBom = new UTF8Encoding(false);
+            using (var reader = new StreamReader(fileLocation, utf8NoBom))
+            {
+                reader.Read();
+                if (Equals(reader.CurrentEncoding, utf8NoBom))
+                {
+                    //Console.WriteLine("No BOM");
+                    using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
 
-            var records = csvReader.GetRecords<ProductCost>();
+                    var records = csvReader.GetRecords<ProductCost>();
+                    return records.ToList();
+                }
+                else
+                {
+                    //Console.WriteLine("BOM detected");
 
-            return records.ToList();
+                    using var csvReader = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.CurrentCulture) { 
+                        Delimiter = ";",
+                        Encoding = GetEncoding(fileLocation)
+                    });
+                    var records = csvReader.GetRecords<ProductCost>();
+                    return records.ToList();
+                }
+            }
+            
+
         }
 
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endian-ness fails.
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        private static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
 
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; // UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; // UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+            return Encoding.Default; // **Changed this line**
+        }
 
     }
 }
