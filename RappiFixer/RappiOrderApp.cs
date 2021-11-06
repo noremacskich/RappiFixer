509469ffac4764ed77f03b0813bce196b2810cbb
NoremacSkich <noremacskich@gmail.com>
2021-11-06 17:18:29 -0500

    Moved the loading in of files into use cases

--- RappiFixer/RappiOrderApp.cs
+++ RappiFixer/RappiOrderApp.cs
@@ -1,12 +1,7 @@
-﻿using CsvHelper;
-using RappiFixer.Models;
-using RappiFixer.UseCases;
+﻿using RappiFixer.UseCases;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace RappiFixer
 {
@@ -14,8 +9,8 @@ namespace RappiFixer
     {
         public void StartApp(string[] args)
         {
-            var allRecords = LoadInCSVFile(args[0]);
-            var productCosts = LoadInProductCostsCSV(args[1]);
+            var allRecords = LoadInRappiCSVFileUseCase.LoadInCSVFile(args[0]);
+            var productCosts = LoadInProductCostsUseCase.LoadInProductCostsCSV(args[1]);
 
             var uniqueRecords = allRecords
                 .GroupBy(x => x.order_id)
@@ -78,85 +73,5 @@ namespace RappiFixer
 
             return menuId;
         }
-
-        private List<CSVHeaders> LoadInCSVFile(string fileLocation)
-        {
-
-            if (!File.Exists(fileLocation))
-            {
-                Console.WriteLine("Unable to find the csv file");
-                throw new FileNotFoundException("unable to find the csv file", fileLocation);
-            }
-
-            using StreamReader reader = new StreamReader(fileLocation);
-            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
-
-            var records = csvReader.GetRecords<CSVHeaders>();
-
-            return records.ToList();
-        }
-
-
-        private List<ProductCost> LoadInProductCostsCSV(string fileLocation)
-        {
-
-            if (!File.Exists(fileLocation))
-            {
-                Console.WriteLine("Unable to find the csv file");
-                throw new FileNotFoundException("unable to find the csv file", fileLocation);
-            }
-
-            var utf8NoBom = new UTF8Encoding(false);
-            using (var reader = new StreamReader(fileLocation, utf8NoBom))
-            {
-                reader.Read();
-                if (Equals(reader.CurrentEncoding, utf8NoBom))
-                {
-                    //Console.WriteLine("No BOM");
-                    using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
-
-                    var records = csvReader.GetRecords<ProductCost>();
-                    return records.ToList();
-                }
-                else
-                {
-                    //Console.WriteLine("BOM detected");
-
-                    using var csvReader = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.CurrentCulture) { 
-                        Delimiter = ";",
-                        Encoding = GetEncoding(fileLocation)
-                    });
-                    var records = csvReader.GetRecords<ProductCost>();
-                    return records.ToList();
-                }
-            }
-            
-
-        }
-
-        /// <summary>
-        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
-        /// Defaults to ASCII when detection of the text file's endian-ness fails.
-        /// </summary>
-        /// <param name="filename">The text file to analyze.</param>
-        /// <returns>The detected encoding.</returns>
-        private static Encoding GetEncoding(string filename)
-        {
-            // Read the BOM
-            var bom = new byte[4];
-            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                file.Read(bom, 0, 4);
-            }
-
-            // Analyze the BOM
-            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
-            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
-            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; // UTF-16LE
-            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; // UTF-16BE
-            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
-            return Encoding.Default; // **Changed this line**
-        }
-
     }
 }
---
+++ RappiFixer/UseCases/LoadInRappiCSVFileUseCase.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using RappiFixer.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace RappiFixer.UseCases
+{
+    public class LoadInRappiCSVFileUseCase
+    {
+        internal static List<CSVHeaders> LoadInCSVFile(string fileLocation)
+        {
+
+            if (!File.Exists(fileLocation))
+            {
+                Console.WriteLine("Unable to find the csv file");
+                throw new FileNotFoundException("unable to find the csv file", fileLocation);
+            }
+
+            using StreamReader reader = new StreamReader(fileLocation);
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csvReader.GetRecords<CSVHeaders>();
+
+            return records.ToList();
+        }
+    }
+}
