4c2c23f482d312d57102258ec9e333af0a4c442f
NoremacSkich <noremacskich@gmail.com>
2021-11-06 16:55:52 -0500

    Profit should now match up better.  Output is now formatted better

--- RappiFixer/UseCases/PrintOutSoldInventoryUseCase.cs
+++ RappiFixer/UseCases/PrintOutSoldInventoryUseCase.cs
@@ -18,22 +18,25 @@ namespace RappiFixer.UseCases
                     ProductName = x.First().product,
                     Price = x.Sum(x => x.product_total_price_with_discount),
                     Count = x.Count(),
-                    Profit = (productCosts.FirstOrDefault(y => y.PROMOCION.Equals(x.First().product, StringComparison.InvariantCultureIgnoreCase))?.GANACIA ?? 0) * x.Count()
+                    Profit = (productCosts.FirstOrDefault(y => y.PROMOCION.Trim().Equals(x.First().product, StringComparison.InvariantCultureIgnoreCase))?.GANACIA ?? 0) * x.Count()
                 }).ToList();
 
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("==================================================================================");
 
+            const int costSpacing = 12;
+            const int countSpacing = 4;
+
             foreach (var product in products)
             {
-                Console.WriteLine($"{product.Count} \t {product.Price:c} \t {product.Profit:c} \t {product.ProductName}");
+                Console.WriteLine($"{product.Count,countSpacing} {product.Price,costSpacing:C} {product.Profit,costSpacing:C} \t {product.ProductName}");
             }
 
 
             Console.WriteLine("==================================================================================");
 
-            Console.WriteLine($"\t {products.Sum(x => x.Price):c} \t {products.Sum(x => x.Profit):c}");
+            Console.WriteLine($"{products.Sum(x => x.Count), countSpacing} {products.Sum(x => x.Price),costSpacing:c} {products.Sum(x => x.Profit),costSpacing:c}");
 
         }
     }
