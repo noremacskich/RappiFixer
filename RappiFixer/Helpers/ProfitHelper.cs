97d032c424d1d0023d16255b6e99865064d6967e
Noremac Skich <noremacskich@gmail.com>
2021-11-28 18:53:43 -0600

    Created override of Print Out Profits for rappi data

--- RappiFixer/Helpers/ProfitHelper.cs
+++ RappiFixer/Helpers/ProfitHelper.cs
@@ -39,5 +39,38 @@ namespace RappiFixer.Helpers
 
             Console.WriteLine($"{products.Sum(x => x.Count),countSpacing} {products.Sum(x => x.Cost),costSpacing:c} {products.Sum(x => x.Profit),costSpacing:c} Retorno Porcentual: {products.Sum(x => x.Profit)/products.Sum(x => x.Cost),4:P} ");
         }
+
+        public static void PrintOutProfits(List<RappiDataModel> allRecords, List<ProductCost> productCosts)
+        {
+            var products = allRecords
+                .GroupBy(x => x.ProductName)
+                .Select(x => new InventoryRow()
+                {
+                    ProductName = x.First().ProductName,
+                    Tipo = (productCosts.FirstOrDefault(y => y.PROMOCION.Trim().Equals(x.First().ProductName, StringComparison.InvariantCultureIgnoreCase))?.TIPO ?? string.Empty),
+                    Cost = x.Sum(x => x.Cost),
+                    Count = x.Sum(x => x.NumberOfUnits),
+                    Profit = (productCosts.FirstOrDefault(y => y.PROMOCION.Trim().Equals(x.First().ProductName, StringComparison.InvariantCultureIgnoreCase))?.GANACIA ?? 0) * x.Sum(x => x.NumberOfUnits)
+                }).ToList();
+
+
+            const int costSpacing = 12;
+            const int countSpacing = 9;
+
+
+            Console.WriteLine($"=================================================================================={new string('=', 30)}");
+            Console.WriteLine($"{"Cantidad",countSpacing} {"Costo",costSpacing} {"Lucro",costSpacing}  {"Tipo",-30} \t Nombre");
+            Console.WriteLine($"=================================================================================={new string('=', 30)}");
+
+            foreach (var product in products.OrderByDescending(x => x.Count).ThenBy(x => x.ProductName))
+            {
+                Console.WriteLine($"{product.Count,countSpacing} {product.Cost,costSpacing:C} {product.Profit,costSpacing:C}  {product.Tipo, -30} \t {product.ProductName}");
+            }
+
+
+            Console.WriteLine($"=================================================================================={new string('=', 30)}");
+
+            Console.WriteLine($"{products.Sum(x => x.Count),countSpacing} {products.Sum(x => x.Cost),costSpacing:c} {products.Sum(x => x.Profit),costSpacing:c} Retorno Porcentual: {products.Sum(x => x.Profit)/products.Sum(x => x.Cost),4:P} ");
+        }
     }
 }
